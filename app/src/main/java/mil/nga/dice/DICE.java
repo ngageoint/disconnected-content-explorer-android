c67ff51b5ae1fbd196c5cab72ab57d2c828976d5
restjohn <rstjohn@caci.com>
2015-04-08 08:20:24 -0600

    enabled WebView debugging when the app is debuggable

--- app/src/main/java/mil/nga/dice/DICE.java
+++ app/src/main/java/mil/nga/dice/DICE.java
@@ -1,9 +1,11 @@
 package mil.nga.dice;
 
 import android.app.Application;
-import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
+import android.webkit.WebView;
 
 import com.google.android.gms.maps.MapsInitializer;
 
@@ -18,6 +20,12 @@ public class DICE extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
+                WebView.setWebContentsDebuggingEnabled(true);
+            }
+        }
 
         File appRoot = getExternalFilesDir(null);
         if (appRoot == null) {
